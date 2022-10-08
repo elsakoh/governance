505a059a6ce1c60bfce10944487c3c500d073e19
elsa <elsayeahx3@gmail.com>
2022-10-09 00:10:37 +0800

    fix: token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,9 +1,4 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-
-contract Token is Ea// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -30,39 +25,16 @@ contract VegeCoin is ERC20, Ownable, ERC20Permit, ERC20Votes {
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
-        super._burn(account, amount);
-    }
-}
-
-        string memory _name,
-        string memory _symbol,
-        uint256 _initialSupply
-    ) ERC20(_name, _symbol) ERC20Permit(_name) {
-        _mint(msg.sender, _initialSupply);
-    }
-
-    // The functions below are overrides required by Solidity.
-
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20Votes) {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
         super._mint(to, amount);
     }
 
     function _burn(address account, uint256 amount)
         internal
-        override(ERC20Votes)
+        override(ERC20, ERC20Votes)
     {
         super._burn(account, amount);
     }
